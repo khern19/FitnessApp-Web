9e8a84f76dc3be1d6001930059b07ba49fd5af2b
klaudia <hernandezklaudia19@gmail.com>
2020-07-11 15:50:17 -0700

    fixed syntax error

--- FitnessApp/frontend/src/components/layout/Header.js
+++ FitnessApp/frontend/src/components/layout/Header.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
 
-export className Header extends Component {
+export class Header extends Component {
     render() {
         return (
-                <nav className="navbar navbar-dark bg-dark">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a className="navbar-brand" href="#">Fitness App</a>
-                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                       
-                        </ul>
-                    </div>
-                </nav>
-           
-        }
+            <nav className="navbar navbar-dark bg-dark">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarTogglerDemo01"
+                    aria-controls="navbarTogglerDemo01"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarTogglerDemo01"
+                >
+                    <a className="navbar-brand" href="#">
+                        Fitness App
+                    </a>
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+                </div>
+            </nav>
+        )
     }
 }
 
