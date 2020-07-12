2cebea2a1b54623948e5b2cd8341796e57f4763b
klaudia <hernandezklaudia19@gmail.com>
2020-07-11 17:14:50 -0700

    adding bootstrap

--- FitnessApp/frontend/src/components/layout/Header.js
+++ FitnessApp/frontend/src/components/layout/Header.js
@@ -3,27 +3,21 @@ import React, { Component } from 'react'
 export class Header extends Component {
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">
+                    Fitness App
+                </a>
                 <button
                     className="navbar-toggler"
                     type="button"
                     data-toggle="collapse"
-                    data-target="#navbarTogglerDemo01"
-                    aria-controls="navbarTogglerDemo01"
+                    data-target="#navbarColor02"
+                    aria-controls="navbarColor02"
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarTogglerDemo01"
-                >
-                    <a className="navbar-brand" href="#">
-                        Fitness App
-                    </a>
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
-                </div>
             </nav>
         )
     }
