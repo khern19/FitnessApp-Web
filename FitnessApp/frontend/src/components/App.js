508b67e194571f12f4040f14cff8ca1b66da508e
klaudia <hernandezklaudia19@gmail.com>
2020-07-12 15:29:10 -0700

    added provider

--- FitnessApp/frontend/src/components/App.js
+++ FitnessApp/frontend/src/components/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
+
+import { Provider } from 'react-redux'
+import store from '../store'
 
 import Header from './layout/Header'
 
 class App extends Component {
     render() {
-        return <Header />
+        return (
+            <Provider store={store}>
+                <Fragment>
+                    <Header />
+                    <div></div>
+                </Fragment>
+            </Provider>
+        )
     }
 }
 
