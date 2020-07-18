ea234106be49879742c4729c8c68ede9eb676b38
klaudia <hernandezklaudia19@gmail.com>
2020-07-18 14:03:03 -0700

    added UsersViewSet and logout

--- FitnessApp/accounts/urls.py
+++ FitnessApp/accounts/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path, include
-from .api import RegisterAPI
+from rest_framework import routers
+from rest_framework import viewsets, permissions
+from .api import RegisterAPI, LoginAPI, UserAPI, UsersViewSet
 from knox import views as knox_views
 
 urlpatterns = [
     path('api/auth', include('knox.urls')),
-    path('api/auth/register', RegisterAPI.as_view())
+    path('api/auth/register', RegisterAPI.as_view()),
+    path('api/auth/login', LoginAPI.as_view()),
+    path('api/auth/user', UserAPI.as_view()),
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
+
+router = routers.DefaultRouter()
+router.register('api/auth/users', UsersViewSet, 'user')
+urlpatterns += router.urls
