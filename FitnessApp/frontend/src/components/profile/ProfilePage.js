2b74f2a2dae194934c5d3562efe1fa7adc38566a
klaudia <hernandezklaudia19@gmail.com>
2020-07-20 16:41:37 -0700

    holder page

---
+++ FitnessApp/frontend/src/components/profile/ProfilePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ProfilePage extends Component {
+    render() {
+        return (
+            <div className="standin">
+                <h1>COMING SOON TO A DESKTOP NEAR YOU</h1>
+            </div>
+        )
+    }
+}
+
+export default ProfilePage
