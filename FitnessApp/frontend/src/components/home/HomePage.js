0380bd790d2fcb8ad8689735321d6d50fe4cb34f
klaudia <hernandezklaudia19@gmail.com>
2020-07-20 16:41:25 -0700

    updated style name

--- FitnessApp/frontend/src/components/home/HomePage.js
+++ FitnessApp/frontend/src/components/home/HomePage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class HomePage extends Component {
     render() {
         return (
-            <div className="homepage">
+            <div className="standin">
                 <h1>COMING SOON TO A DESKTOP NEAR YOU</h1>
             </div>
         )
