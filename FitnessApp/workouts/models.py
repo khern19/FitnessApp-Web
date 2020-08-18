f5a0cf5bd9a55ad170a91076cb222811e445a339
klaudia <hernandezklaudia19@gmail.com>
2020-08-18 15:58:08 -0700

    added workout info to db

--- FitnessApp/workouts/models.py
+++ FitnessApp/workouts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Workouts(models.Model):
+    workout_name = models.CharField(max_length=150)
+    repsc = models.PositiveSmallIntegerField(default=0)
+    sets = models.PositiveSmallIntegerField(default=0)
+    distance = models.DecimalField(max_digits=9, decimal_places=6)
+    time = models.DecimalField(max_digits=9, decimal_places=6)
+    date = models.DateField(blank=True, null=True)
