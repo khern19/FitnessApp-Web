3dc50e3ef467237b4784999ee474d6db5e9db6ef
klaudia <hernandezklaudia19@gmail.com>
2020-08-19 16:01:17 -0700

    created html file

--- FitnessApp/frontend/src/components/App.js
+++ FitnessApp/frontend/src/components/App.js
@@ -1,3 +1,11 @@
 import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+
+export class App extends Component {
+    render() {
+        return <h1>Hello</h1>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
