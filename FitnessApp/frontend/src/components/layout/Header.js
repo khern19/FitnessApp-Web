499327265f61068f126060705cf208fdd8ad3bf9
klaudia <hernandezklaudia19@gmail.com>
2020-08-21 14:44:27 -0700

    fixed return error

--- FitnessApp/frontend/src/components/layout/Header.js
+++ FitnessApp/frontend/src/components/layout/Header.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react'
 
 export class Header extends Component {
     render() {
-        return
-        ;<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">
-                Fitness App
-            </a>
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarColor02"
-                aria-controls="navbarColor02"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
-        </nav>
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">
+                    Fitness App
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarColor02"
+                    aria-controls="navbarColor02"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </nav>
+        )
     }
 }
 
