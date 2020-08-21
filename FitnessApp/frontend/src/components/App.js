e40ad967c51faeb8ca9ded887b0bca59f327aa35
klaudia <hernandezklaudia19@gmail.com>
2020-08-21 14:38:54 -0700

    added header to show

--- FitnessApp/frontend/src/components/App.js
+++ FitnessApp/frontend/src/components/App.js
@@ -2,9 +2,11 @@ import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
+import Header from './layout/Header'
+
 export class App extends Component {
     render() {
-        return <h1>Hello</h1>
+        return <Header />
     }
 }
 
