e88eb92f18a81792c989caebc05c9c85aaccc46f
klaudia <hernandezklaudia19@gmail.com>
2020-08-28 15:25:56 -0700

    fixed reps name

--- FitnessApp/workouts/models.py
+++ FitnessApp/workouts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Workouts(models.Model):
     workout_name = models.CharField(max_length=150)
-    repsc = models.PositiveSmallIntegerField(default=0)
+    reps = models.PositiveSmallIntegerField(default=0)
     sets = models.PositiveSmallIntegerField(default=0)
     distance = models.DecimalField(max_digits=9, decimal_places=6)
     time = models.DecimalField(max_digits=9, decimal_places=6)
